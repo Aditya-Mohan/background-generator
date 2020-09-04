e791d24b8e4c4ffd47d14935eef70c32fb407d2b
Aditya <aditya530026@gmail.com>
2020-09-04 15:12:56 +0530

    addind start project

---
+++ script.js
@@ -0,0 +1,16 @@
+let hexCode = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
+
+function setGradient() {
+	body.style.background = 
+	`linear-gradient(90deg,${color1.value}, ${color2.value})`;
+
+	hexCode.textContent = `${body.style.background};`;
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
